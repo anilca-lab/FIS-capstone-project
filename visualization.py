d2d34fcca6da49e8aa834a7a5394130253fb3c7b
Anil Onal <anilca@gmail.com>
2020-01-16 09:41:10 -0500

    slight modeling changes

---
+++ visualization.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 22:27:08 2020
+
+@author: flatironschol
+"""
+import folium
+
+m = folium.Map(location=[45.5236, -122.6750])
